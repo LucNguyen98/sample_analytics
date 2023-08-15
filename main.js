e5375900eb4898db598240687c9369bd9c5c5536
Nguyen Thanh Luc <luc.nguyen150198@gmail.com>
2023-08-15 07:10:58 +0700

    update params

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function gtagEvent(event_name, object) {
-  window.gtag("event", event_name, object);
+  gtag("event", event_name, object);
 }
 
 gtag("event", "screen_view", {
@@ -8,8 +8,17 @@ gtag("event", "screen_view", {
 });
 
 function addResult() {
-    alert(123)
   gtagEvent("add_risk_result", {
     userID: "123",
+    weight: 70,
+    age:18,
+    gender: 1,
+    sys:100,
+    dia:200,
+    pm:200,
+    "Absolute 10 year stroke risk":'50%',
+    "10-year stroke risk* is increased in": "50%",
+    "Absolute 5-year stroke risk" : "30%",
+    "5-year stroke risk* is increased in" : "10%"
   });
 }
